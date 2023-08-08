58738a5c51cf4aaf4d90d59155ca4158eca253af
umairansari <umairansari.work@gmail.com>
2023-08-09 03:02:30 +0500

    Added gsap parallax effect

--- main.js
+++ main.js
@@ -1,16 +1,36 @@
+// GSAP: Default settings.
+gsap.defaults({ease: "easeinout"});
+
+// GSAP: Scroll pinned effect.
 ScrollTrigger.create({
 	trigger: ".scroll",
 	start: "top top",
 	end: "bottom bottom",
-	pin: ".scroll-right-pinned",
+	pin: ".scroll-right",
     markers: true,
-})
+});
 
+// GSAP: Clipping-mask parallax effect.
+const tl = gsap.timeline();
+
+tl.from(".scroll-right-parallax-ii", { bottom: "-100%" });
+tl.from(".scroll-right-parallax-iii", { bottom: "-100%" });
+tl.from(".scroll-right-parallax-iv", { bottom: "-100%" });
+
+ScrollTrigger.create({
+	animation: tl,
+	trigger: ".scroll",
+	start: "top top",
+	end: "bottom bottom",
+	scrub: true,
+	snap: 1/3,
+});
+
+// Lenis: Smooth scrolling effects.
 const lenis = new Lenis();
 
 function raf(time) {
-  lenis.raf(time)
-  requestAnimationFrame(raf)
+	lenis.raf(time);
+	requestAnimationFrame(raf);
 };
-
 requestAnimationFrame(raf);
